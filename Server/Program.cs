09bcc58d1e10ca1cc0e875e40a6a9f4a0a10fc70
Timo Heikkilä <timo.heikkila@hotmail.com>
2026-07-12 23:25:16 +0200

    Json added, output folder for the dll hard coded

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,14 @@
+using Newtonsoft.Json.Serialization;
 using Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(o =>
+{
+    o.SerializerSettings.ContractResolver = new DefaultContractResolver();
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
